85c8e94e7ee15c7134193bf0689f1fae87c50274
Yingjun Cui <yingjuncui@gmail.com>
2014-07-23 10:25:24 -0400

    fix bug

--- Model/MessagesUser.php
+++ Model/MessagesUser.php
@@ -60,11 +60,14 @@ class MessagesUser extends MessagesAppModel {
 		return $this->save($message, array('callbacks' => false)); 		
  	}
 
-
     public function newMessageCount($userId){
+
+
         return $this->find('count',array(
-            'MessagesUser.user_id' => $userId,
-            'label' =>'unread',
+            'conditions' => array(
+                'MessagesUser.user_id' => $userId,
+                'label' =>'unread',
+            )
         ));
 
 
